e4388cb0d3e0aa95d425bb7fbe863eb9b8031fb2
radu-bordea <radu.bordea.dev@gmail.com>
2023-11-28 14:39:22 +0100

    save to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,27 @@ import Form from "./Form";
 import { nanoid } from "nanoid";
 import Items from "./Items";
 
+// 2 different implementations for getting data from local storage
+
+// const getLocalStorage = () => {
+//   let list = localStorage.getItem("list");
+//   if(list) {
+//     list = JSON.parse(localStorage.getItem('list'));
+//   }
+//   else {
+//     list = []
+//   }
+//   return list;
+// };
+
+const defaultList = JSON.parse(localStorage.getItem("list") || "[]");
+
+const setLocalStorage = (items) => {
+  localStorage.setItem("list", JSON.stringify(items));
+};
+
 const App = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(defaultList);
 
   const addItem = (itemName) => {
     const newItem = {
@@ -12,19 +31,33 @@ const App = () => {
       completed: false,
       id: nanoid(),
     };
-
-    setItems([...items, newItem]);
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
   };
 
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId);
     setItems(newItems);
+    setLocalStorage(newItems);
+  };
+
+  const editItem = (itemId) => {
+    const newItems = items.map((item) => {
+      if (item.id === itemId) {
+        const newItem = { ...item, completed: !item.completed };
+        return newItem;
+      }
+      return item;
+    });
+    setItems(newItems)
+    setLocalStorage(newItems);
   };
 
   return (
     <section className="section-center">
       <Form addItem={addItem} />
-      <Items items={items} removeItem={removeItem} />
+      <Items items={items} removeItem={removeItem} editItem={editItem}/>
     </section>
   );
 };
