e31351933069729f13e68cec4d097ce9e7524e15
radu-bordea <radu.bordea.dev@gmail.com>
2023-11-28 14:44:20 +0100

    add toastify alerts

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Form from "./Form";
 import { nanoid } from "nanoid";
 import Items from "./Items";
+import { ToastContainer, toast } from "react-toastify";
 
 // 2 different implementations for getting data from local storage
 
@@ -34,12 +35,14 @@ const App = () => {
     const newItems = [...items, newItem];
     setItems(newItems);
     setLocalStorage(newItems);
+    toast.success('item added to the list')
   };
 
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId);
     setItems(newItems);
     setLocalStorage(newItems);
+    toast.success('item deleted')
   };
 
   const editItem = (itemId) => {
@@ -56,6 +59,7 @@ const App = () => {
 
   return (
     <section className="section-center">
+    <ToastContainer position="top-center"/>
       <Form addItem={addItem} />
       <Items items={items} removeItem={removeItem} editItem={editItem}/>
     </section>
